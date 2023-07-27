7d035d9620049a077254eeeb6f46b903f261f47d
RKanna <rkannan.balakrishnan@hotmail.com>
2023-07-27 21:47:34 +0530

    Script done for scroll border effect

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 "use strict";
+const navBar = document.getElementById("navBar");
+navBar.style.borderBottom = "none";
+window.addEventListener("scroll", function () {
+  let touchContainer = document.getElementById("touch-container");
+  const navBar = document.getElementById("navBar");
+  let scrollPosition = window.scrollY;
+  let containerTop = touchContainer.offsetTop;
+  let containerHeight = touchContainer.offsetHeight;
+
+  if (scrollPosition >= containerTop) {
+    navBar.style.borderBottom = "2px solid #dcdddc";
+  } else {
+    navBar.style.borderBottom = "none";
+  }
+});
